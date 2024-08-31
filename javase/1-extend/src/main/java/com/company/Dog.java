ac9369f5f677ed06ea743273d3ffe08d09254a73
felix <felixzxy29@gmail.com>
2024-08-31 22:37:15 +0800

    super and override attribute

--- javase/1-extend/src/main/java/com/company/Dog.java
+++ javase/1-extend/src/main/java/com/company/Dog.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public class Dog extends Animal {
+    private String name;
     public void eat() {
         System.out.println(this.getName() + " is eating...");
+    }
+
+    @Override
+    public String getName () {
+        System.out.println("this is in class Dog");
+        return this.name;
+    }
+    @Override
+    public void setName (String name) {
+        this.name = name;
     }
 
 }

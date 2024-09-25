525e106d90d015150bc134292a8603e6eb47c03a
felix <felixbush@proton.me>
2024-09-25 20:59:01 +0800

    feat: exception

---
+++ javase/9-exception/src/com/company/ExceptionApplication.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class ExceptionApplication {
+    public static void main (String[] args) {
+        int[] nums = new int[2];
+        System.out.println(nums[2]);
+
+    }
+}

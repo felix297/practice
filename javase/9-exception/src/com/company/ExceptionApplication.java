740cb1adebf845e316b327161390792bf7d0d373
felix <felixbush@proton.me>
2024-09-26 21:57:01 +0800

    fix(exception): try/catch

--- javase/9-exception/src/com/company/ExceptionApplication.java
+++ javase/9-exception/src/com/company/ExceptionApplication.java
@@ -2,8 +2,15 @@ package com.company;
 
 public class ExceptionApplication {
     public static void main (String[] args) {
-        int[] nums = new int[2];
-        System.out.println(nums[2]);
+        try {
+            int[] nums = new int[2];
+            System.out.println(nums[2]);
+        } catch (Exception e) {
+            // 哄骗毕业设计
+            // e.printStackTrace();
+            System.out.println("message: " + e.getMessage());
+            System.out.println("cause: " + e.getCause().getMessage());
+        }
 
     }
 }

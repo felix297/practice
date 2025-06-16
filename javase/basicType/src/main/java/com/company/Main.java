0fe6c86e951ec2e9f43beed8ed5ff7a92fdb6193
felix <felixbush@proton.me>
2025-06-16 21:28:44 +0800

    feat: value range of basic type of java

---
+++ javase/basicType/src/main/java/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("----BYTE----");
+        System.out.println("Size: " + Byte.SIZE);
+        System.out.println("Max value: " + Byte.MAX_VALUE);
+        System.out.println("Minimum value: " + Byte.MIN_VALUE);
+        System.out.println("----SHORT----");
+        System.out.println("Size: " + Short.SIZE);
+        System.out.println("Maximum Value: " + Short.MAX_VALUE);
+        System.out.println("Minimum Value: " + Short.MIN_VALUE);
+        System.out.println("----INT----");
+        System.out.println("Size: " + Integer.SIZE);
+        System.out.println("Maximum Value: " + Integer.MAX_VALUE);
+        System.out.println("Minimum Value: " + Integer.MIN_VALUE);
+        System.out.println("----LONG----");
+        System.out.println("Size: " + Long.SIZE);
+        System.out.println("Maximum Value: " + Long.MAX_VALUE);
+        System.out.println("Minimum Value: " + Long.MIN_VALUE);
+        System.out.println("----FLOAT----");
+        System.out.println("Size: " + Float.SIZE);
+        System.out.println("Maximum Value: " + Float.MAX_VALUE);
+        System.out.println("Minimum Value: " + Float.MIN_VALUE);
+        System.out.println("----DOUBLE----");
+        System.out.println("Size: " + Double.SIZE);
+        System.out.println("Maximum Value: " + Double.MAX_VALUE);
+        System.out.println("Minimum Value: " + Double.MIN_VALUE);
+        System.out.println("----CHAR----");
+        System.out.println("Size: " + Character.SIZE);
+        System.out.println("Maximum Value: " + Character.MAX_VALUE);
+        System.out.println("Minimum Value: " + Character.MIN_VALUE);
+
+
+    }
+}
